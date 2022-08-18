dd0fab4463a7281cfee4e03bcd30759960d0462c
Fabian Schmidt <fabian@my-schmidt.de>
2022-08-18 16:42:24 +0200

    add bookmarks

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,24 @@
+class BookmarksController < ApplicationController
+  before_action :set_list, only: %i[new create]
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    @bookmark.save
+    redirect_to list_path(@list)
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'lists/index'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :lists, only: [:index, :show]
-
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create]
+  end
 end
